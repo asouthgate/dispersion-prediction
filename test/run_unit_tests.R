0440137bc659938326289190e950282a716348f5
Alex Southgate <joelsouthgate@googlemail.com>
2026-06-18 15:01:02 +0100

    Add main to CI; minor formatting change to logging; add helper file that was missing

--- test/run_unit_tests.R
+++ test/run_unit_tests.R
@@ -2,6 +2,6 @@
 options(project_root = getwd())
 files <- list.files("test/unit", pattern = "^test_.*\\.R$", full.names = TRUE)
 for (f in files) {
-  cat("== Running:", basename(f), "==\n")
+  cat("Running:", basename(f), "...\n")
   source(f)
 }
