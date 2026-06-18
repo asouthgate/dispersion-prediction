97f0f6aee5954b58771bc40ab85dd45516d9c82d
Alex Southgate <joelsouthgate@googlemail.com>
2026-06-18 14:45:16 +0100

    Change to renv and introduce a CI pipeline

---
+++ test/run_unit_tests.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+options(project_root = getwd())
+files <- list.files("test/unit", pattern = "^test_.*\\.R$", full.names = TRUE)
+for (f in files) {
+  cat("== Running:", basename(f), "==\n")
+  source(f)
+}
