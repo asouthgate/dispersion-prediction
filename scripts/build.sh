4363b4d09bbf261c28afb9d9b5c23df88fb3a4d0
Alex Southgate <joelsouthgate@googlemail.com>
2026-07-21 10:41:34 +0100

    Add npm install to build images

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
-cd frontend/gsbio-engine && npm run build && cd ../..
+cd frontend/gsbio-engine && npm install && npm run build && cd ../..
+cd frontend/ && npm install && npm run build && cd ..
 docker build --network=host -t dispersion-prediction-app-frontend -f docker/Dockerfile.frontend . --build-context gsbio=./frontend/gsbio-engine
 docker build -t dispersion-prediction-app-api -f docker/Dockerfile.backend .
 docker build -t dispersion-prediction-app-postgis -f docker/Dockerfile.gis .
