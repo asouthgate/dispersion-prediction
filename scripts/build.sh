8bc70850d5075f6ed3a6bf85bcf6f6d228d4afc9
Alex Southgate <joelsouthgate@googlemail.com>
2026-08-10 12:04:49 +0100

    Add required docker building files required for building rust

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,23 @@
-docker run --rm -v "$(pwd)/frontend/gsbio-engine":/app -w /app node:lts sh -c "npm install && npm run build"
-docker run --rm -v "$(pwd)/frontend":/app -w /app node:lts sh -c "npm install && npm run build"
+docker build -f docker/Dockerfile.frontend.builder -t frontend-builder:latest .
+
+docker run --rm \
+  -v "$(pwd)":/app \
+  -w /app/frontend/wasm-connectivity \
+  frontend-builder:latest \
+  wasm-pack build --target web --out-dir lib
+
+docker run --rm \
+  -v "$(pwd)":/app \
+  -w /app/frontend/gsbio-engine \
+  frontend-builder:latest \
+  sh -c "npm install && npm run build"
+
+docker run --rm \
+  -v "$(pwd)":/app \
+  -w /app/frontend \
+  frontend-builder:latest \
+  sh -c "npm install && npm run build"
+
 docker build --network=host -t dispersion-prediction-app-frontend -f docker/Dockerfile.frontend . --build-context gsbio=./frontend/gsbio-engine
 docker build -t dispersion-prediction-app-api -f docker/Dockerfile.backend .
 docker build -t dispersion-prediction-app-postgis -f docker/Dockerfile.gis .
